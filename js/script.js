6eca1716dce7d6dfd531ecd68562f9af58f3e5f2
rameem2003 <rameem2019@gmail.com>
2022-06-14 15:43:58 +0600

    update design

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 const nav = document.querySelector("nav");
 const tog = document.getElementById("tog");
 
+const header = document.querySelector("header");
+
+
 
 tog.addEventListener("click", () => {
     nav.classList.toggle("active");
 })
+
+
+window.onscroll = () => {
+    nav.classList.remove("active");
+
+    if (document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280) {
+        header.classList.add("scroll");
+    } 
+    else{
+        header.classList.remove("scroll");
+    }
+}
